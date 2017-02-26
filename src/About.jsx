ad4dea5ef1625b4b1d223daf7b8ac7064af98d66
Victor Mours <victor.mours@gmail.com>
2017-02-26 13:11:33 +0100

    Use arrow syntax for components

--- src/About.jsx
+++ src/About.jsx
@@ -3,13 +3,11 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import Layout from './Layout'
 import s from './style.css'
 
-function About(props) {
-  return (
-    <Layout>
-      <h1>About</h1>
-      <p>This site is powered by vitaminjs. Hurray!</p>
-    </Layout>
-  );
-}
+const About = (props) => (
+  <Layout>
+    <h1>About</h1>
+    <p>This site is powered by vitaminjs. Hurray!</p>
+  </Layout>
+);
 
 export default withStyles(s)(About);
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,19 +4,17 @@ import Helmet from 'react-helmet';
 import { Link } from 'react-router'
 import s from './style.css'
 
-function Layout(props) {
-  return (
-    <div className={s.app}>
-      <Helmet
-        title="VitaminJS"
-        meta={[
-          { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        ]}
-      />
-      <Link to="/">Home</Link>
-      { props.children }
-    </div>
-  );
-}
+const Layout = (props) => (
+  <div className={s.app}>
+    <Helmet
+      title="VitaminJS"
+      meta={[
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ]}
+    />
+    <Link to="/">Home</Link>
+    { props.children }
+  </div>
+);
 
 export default withStyles(s)(Layout);
